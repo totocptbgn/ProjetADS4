ee9d51fd7bf82c37640ff88fcc2cecc7bba10b8c
Thomas Copt-Bignon <lowx.add@gmail.com>
2019-04-17 17:40:01 +0200

    Ajout des fonctions de test

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         runMain(args);
-        runTests(args);
+        runTests();
     }
 
     /**
-     * Fonction qui parse et interprete le programme
-     *
+     * Fonction qui parse et interprete le programme.
      * @param args Tableau de string indiquant l'adresse de la grille et du programme à interpreter
      * @throws IOException Quand le parser ne trouve pas un symbole autorisé par la grammaire
      */
@@ -21,10 +18,11 @@ public class Main {
         IOEnv ioEnv = IOEnv.parseArgs(exeName, args);
 
         // Création du Parser
-        Parser parser = new SmartParser(new FileReader(new File(args[0])));
+        Parser parser = new SmartParser(ioEnv.inProgram);
 
         // Construction du Program en lisant le ficher grâce au Parser
         Program prog = parser.parseProgram(exeName, ioEnv.inProgram);
+        System.out.println(prog);
         Grid grid = Grid.parseGrid(exeName, ioEnv.inGrid);
 
         // Intrepetation et execution du programme sur la grille
@@ -39,14 +37,43 @@ public class Main {
 
     /**
      * Fonction qui teste les différents programme de test.
-     *
-     * @param args Tableau de string indiquant l'adresse de la grille et du programme à interpreter
-     * @throws IOException Quand le parser ne trouve pas un symbole autorisé par la grammaire.
+     * @throws FileNotFoundException Levée lorsqu'un des fichiers à tester n'est pas trouvé.
      */
 
-    private static void runTests(String[] args) throws IOException {
-        String exeName = "Tests";
-        IOEnv ioEnv = IOEnv.parseArgs(exeName, args);
+    private static void runTests() {
+        System.out.println("Test des fichiers dans le répertoire tests :\n");
+        testFile("good0");
+        testFile("good1");
+        testFile("good2");
+        testFile("bad0");
+        testFile("bad1");
+        testFile("bad2");
+    }
 
+    /**
+     * Fonction qui test si un fichier est correct, c'est-à-dire, respecte la grammaire du SmartParser).
+     * @param filename Nom du fichier dans src/tests/ à tester.
+     */
+
+    private static void testFile(String filename) {
+        System.out.print("-  Test de " + filename + " : ");
+
+        // Création du Reader sur le fichier indiqué avec filename
+        Reader reader;
+        try {
+            reader = new FileReader(new File("src/tests/" + filename));
+        } catch (FileNotFoundException e) {
+            System.out.println("Fichier non trouvé.");
+            return;
+        }
+
+        // Parsing du fichier et affichage des résultats
+        Parser parser = new SmartParser(reader);
+        try {
+            parser.parseProgram("Tests", reader);
+            System.out.println("Fichier correct.");
+        } catch (IOException e) {
+            System.out.println("Fichier incorrect. Cause: [" + e.getMessage() + "]");
+        }
     }
 }
